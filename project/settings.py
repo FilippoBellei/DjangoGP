02febc71ab8dc5500f477100508282f6a552c77d
Filippo Bellei <filippobellei@gmail.com>
2023-03-28 10:35:27 +0200

    Add Vercel support

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-+2jx&q7a8b_kjj#!gx@cikr3u5)o_i&f+9d(d$jw%nkow8ww15
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app"]
 
 
 # Application definition
