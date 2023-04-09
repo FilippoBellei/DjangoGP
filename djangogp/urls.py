5138cfa9a612ddbdf35c0f3aa8cbbb752d6a3a18
Filippo Bellei <filippobellei@gmail.com>
2023-04-09 15:40:49 +0200

    Update urls.py

--- djangogp/urls.py
+++ djangogp/urls.py
@@ -4,11 +4,11 @@ from djangogp.views import *
 app_name = "djangogp"
 
 urlpatterns = [
-    path("riders/", RiderListView.as_view(), name="rider_list"),
+    path("", RiderListView.as_view(), name="rider_list"),
     path("rider/<int:pk>/", RiderDetailView.as_view(), name="rider_detail"),
     path("rider/<int:pk>/update/", RiderUpdateView.as_view(), name="rider_update"),
     path("rider/create/", RiderCreateView.as_view(), name="rider_create"),
     path("rider/<int:pk>/delete/", RiderDeleteView.as_view(), name="rider_delete"),
-    path("races", RaceRedirectView.as_view(), name="race_list"),
+    path("races/", RaceRedirectView.as_view(), name="race_list"),
     path("race/<int:pk>/", RaceDetailView.as_view(), name="race_detail"),
 ]
