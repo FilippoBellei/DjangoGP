33e2f5fecb688dd03a3364624c67fca73be3dfab
Filippo Bellei <filippobellei@gmail.com>
2023-04-19 15:13:34 +0200

    Add secrets

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from project.secrets import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-+2jx&q7a8b_kjj#!gx@cikr3u5)o_i&f+9d(d$jw%nkow8ww15"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
